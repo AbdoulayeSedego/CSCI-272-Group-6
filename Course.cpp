9786aff191121deca6c43c13e9ddf201eb2d4047
abdoulayesedego <abdoulaye.sedego95@gmail.com>
2025-10-14 02:06:01 -0400

    setup project includes

---
+++ Course.cpp
@@ -0,0 +1,10 @@
+#include "header/Course.h"
+#include "header/Student.h"
+#include <iomanip>
+#include <string>
+#include <iostream>
+#include <vector>
+
+int ad(int a,int b) {
+    return a + b;
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "header/Course.h"
+#include "header/Student.h"
+#include "Course.cpp"
+#include <iomanip>
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main () {
+  int result = ad(7, 9);
+
+  cout << result << endl;
+
+}
